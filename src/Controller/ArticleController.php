36d46f96d74a151a69d97740bdd375be04550c79
Marie-Céline PEDURAND <marieceline.pedurand@gmail.com>
2023-08-27 13:46:12 +0200

    Intégration des données des articles dans la section 'À la une du Mag' de la page d'accueil

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Article;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ArticleController extends AbstractController
 {
@@ -15,4 +17,26 @@ class ArticleController extends AbstractController
             'controller_name' => 'ArticleController',
         ]);
     }
+
+    #[Route('/articles', name: 'article_all')]
+    public function all(): Response
+    {
+        return $this->render('article/all.html.twig', [
+            'controller_name' => 'ArticleController',
+        ]);
+    }
+
+    public function recent(EntityManagerInterface $entityManager): Response
+    {
+        $article = $entityManager->getRepository(Article::class)
+            ->findOneBy([], ['date' => 'DESC']);
+
+        if (!$article) {
+            return $this->redirectToRoute('home');  // Redirect to home page if no article found
+        }
+
+        return $this->render('article/_recent.html.twig', [
+            'article' => $article,
+        ]);
+    } // The list method uses the EntityManager service to retrieve articles sorted by date in 'descending order', and then passes them to the articles/_recent.html.twig template.
 }
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,28 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Article;
+use App\Repository\MediaRepository;
+use App\Repository\ArticleRepository;
+use App\Repository\CommentRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager, ArticleRepository $articleRepo, MediaRepository $mediaRepo): Response
     {
+        $article = $entityManager->getRepository(Article::class)
+            ->findOneBy([], ['createdAt' => 'DESC']);
+
+        $media = $mediaRepo->findOneBy(['mediaOwnerId' => $article->getId()]);
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'articles' => $articleRepo->findAll(),
+            'media' => $media,
         ]);
     }
 }
